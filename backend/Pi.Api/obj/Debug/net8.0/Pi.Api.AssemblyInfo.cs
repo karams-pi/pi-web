ed57e53e379c94b7cbd0cef95d2d85d90cdf2f81
Paulo Takeda <paulo.takeda@agililondrina.local>
2026-01-21 16:37:30 -0300

    feat: criação da tabela lista_preco

--- backend/Pi.Api/obj/Debug/net8.0/Pi.Api.AssemblyInfo.cs
+++ backend/Pi.Api/obj/Debug/net8.0/Pi.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce060a272941072639e35cb0426b1052a78a2dd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77b4a09cce53cf1055f3dd7c9aef4bbb6511cda8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
