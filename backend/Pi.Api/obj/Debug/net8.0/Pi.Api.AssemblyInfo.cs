5e81b622b8089d3cfd764afd30e2069f211321e4
= <=>
2026-01-25 17:15:44 -0300

    alteração na tela de módulo

--- backend/Pi.Api/obj/Debug/net8.0/Pi.Api.AssemblyInfo.cs
+++ backend/Pi.Api/obj/Debug/net8.0/Pi.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+611212ee1ec92df01dce6edd3c90be9d6b54a07f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf73855621384c79fc92d789fc81f300291c99d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
