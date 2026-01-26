55569317ed3c31d93374a0c5fa13c3fb845e7abf
Paulo Takeda <paulo.takeda@agililondrina.local>
2026-01-26 15:19:47 -0300

    feat: Implement Freight Items CRUD, UI Refactor and DB Sequence Fix

--- backend/Pi.Api/obj/Debug/net8.0/Pi.Api.AssemblyInfo.cs
+++ backend/Pi.Api/obj/Debug/net8.0/Pi.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a702e066905344d60d14b891800f357b8c731593")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2cc6a3ee04dc396cdc7be64541e332346ee3165")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
