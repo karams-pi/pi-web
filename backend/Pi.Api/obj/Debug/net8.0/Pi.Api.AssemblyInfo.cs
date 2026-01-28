a4c3220350892258d9e751f46297b2d5a4f90002
= <=>
2026-01-28 12:19:52 -0300

    sei la

--- backend/Pi.Api/obj/Debug/net8.0/Pi.Api.AssemblyInfo.cs
+++ backend/Pi.Api/obj/Debug/net8.0/Pi.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9dcec4d3e463eb5ffe5ed61c70b16f23dea1a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d50c1f72eaeb247ec27adde837ed58eee6c8142")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
