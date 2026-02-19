7150b01fd74d24a2d54a26e7f1db361e90a89689
Paulo Takeda <paulo.takeda@agililondrina.local>
2026-02-19 15:54:01 -0300

    teste

--- backend/Pi.Api/obj/Debug/net8.0/Pi.Api.AssemblyInfo.cs
+++ backend/Pi.Api/obj/Debug/net8.0/Pi.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c790e568026d9a6b16e512c0327bf20f189513a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1965f6d5874863c3a00d0f184779bccebdf35cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pi.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
